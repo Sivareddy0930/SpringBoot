4cecc5dacceca9d1785340b28728ea050ea0275a
Sivareddy0930 <sivareddy0930@gmail.com>
2024-08-08 11:47:14 +0530

    Api documentation using springdoc-opeanai internal using swagger ui

--- BasicSpringBootProject2/src/main/java/com/BasicSpringBootProject2/Contoller/EmployeeController.java
+++ BasicSpringBootProject2/src/main/java/com/BasicSpringBootProject2/Contoller/EmployeeController.java
@@ -3,13 +3,14 @@ package com.BasicSpringBootProject2.Contoller;
 
 import com.BasicSpringBootProject2.Dto.EmployeeDto;
 import com.BasicSpringBootProject2.service.EmployeeService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
 
 
@@ -26,18 +27,43 @@ public class EmployeeController {
 
     //createEmployee Api
 
+    @Operation(
+            summary = "create Employee Rest Api",
+            description = "create Employee Rest Api used to save employee object in Database"
+    )
+    @ApiResponse(
+            responseCode = "201",
+            description = "Http status 201 Created"
+    )
     @PostMapping("/createEmployee")
     public ResponseEntity<EmployeeDto> createEmployee(@Valid @RequestBody EmployeeDto employeeDto){
         EmployeeDto empDto = employeeService.createEmployee(employeeDto);
         return new ResponseEntity(empDto, HttpStatus.CREATED);
     }
 
+
+    @Operation(
+            summary = "getEmployeeById  Rest Api",
+            description = "getEmployeeById  Rest Api used to get employee object by id from Database"
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "Http status 200 Success"
+    )
     @GetMapping("/getEmployee/{id}")
     public ResponseEntity<EmployeeDto> getEmployeeById(@PathVariable("id") Integer empId){
         EmployeeDto empDto = employeeService.getEmployeeById(empId);
         return new ResponseEntity(empDto, HttpStatus.OK);
     }
 
+    @Operation(
+            summary = "getAllEmployee  Rest Api",
+            description = "getAllEmployee  Rest Api used to get All employee object  from Database"
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "Http status 200 Success"
+    )
     @GetMapping("/getAllEmployee")
     public ResponseEntity<List<EmployeeDto>> getAllEmployee(){
         List<EmployeeDto> empDtoList = employeeService.getAllEmployee();
@@ -46,12 +72,29 @@ public class EmployeeController {
 
 
 
+    @Operation(
+            summary = "updateEmployee Rest Api",
+            description = "updateEmployee  Rest Api used to update employee object in the Database"
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "Http status 200 Success"
+    )
     @PutMapping("/updateEmployee/{id}")
     public ResponseEntity<EmployeeDto> updateEmployeeById(@PathVariable Integer id, @RequestBody @Valid EmployeeDto employeeDto){
         EmployeeDto empDto = employeeService.updateEmployeeById(id,employeeDto);
         return new ResponseEntity(empDto, HttpStatus.OK);
     }
 
+
+    @Operation(
+            summary = "deleteEmployee Rest Api",
+            description = "deleteEmployee Rest Api used to delete employee object in the Database"
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "Http status 200 Success"
+    )
 
     @DeleteMapping("/deleteEmployee/{id}")
     public ResponseEntity<String> deleteEmployeeById(@PathVariable Integer id){
--- BasicSpringBootProject2/src/main/java/com/BasicSpringBootProject2/Dto/EmployeeDto.java
+++ BasicSpringBootProject2/src/main/java/com/BasicSpringBootProject2/Dto/EmployeeDto.java
@@ -1,5 +1,6 @@
 package com.BasicSpringBootProject2.Dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
@@ -12,17 +13,29 @@ import lombok.Setter;
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
+@Schema(
+        description = "Employee Dto management Information"
+)
 public class EmployeeDto {
 
     private Integer id;
 
     @NotEmpty(message = "Name can't be empty!,")
+    @Schema(
+            description = "Employee name"
+    )
     private String name;
 
     @NotEmpty(message = "E-mail can't be empty")
     @Email
+    @Schema(
+            description = "Employee email"
+    )
     private String email;
 
     @NotNull(message = "Salary can't be empty!,")
+    @Schema(
+            description = "Employee salary"
+    )
     private Double salary;
 }
